869d9649d3ab11acc365d4a8aaf2b788f432cc24
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2023-10-14 15:12:43 -0700

    add index_mongo, db_con, wip

--- mongodb/db_connection_mongo.py
+++ mongodb/db_connection_mongo.py
@@ -12,57 +12,87 @@
 #importing some Python libraries
 # --> add your Python code here
 
+
+
+import string
+from pymongo import MongoClient
+
+
 def connectDataBase():
 
     # Create a database connection object using psycopg2
     # --> add your Python code here
+    client = MongoClient(host=['localhost:27017'])
+    db = client["documents"]
+    collection_cursor = db["document"]
+    
+    return collection_cursor
 
-def createCategory(cur, catId, catName):
-
-    # Insert a category in the database
-    # --> add your Python code here
 
 def createDocument(cur, docId, docText, docTitle, docDate, docCat):
-
-    # 1 Get the category id based on the informed category name
-    # --> add your Python code here
+    
 
     # 2 Insert the document in the database. For num_chars, discard the spaces and punctuation marks.
     # --> add your Python code here
+    
+    # count number of chars, excluding spaces
+    docText_noSpaces = docText.replace(" ","")
+    docText_punc = docText_noSpaces.translate(str.maketrans('', '', string.punctuation))
+    num_chars = len(docText_punc)
+    
+    # get list of terms
+    docText_clearPunc = docText.translate(str.maketrans('', '', string.punctuation))
+    docText_lower = docText_clearPunc.lower()
+    docText_terms = docText_lower.split()
+    
+    term_map = {}
+    for term in docText_terms:
+        if term in term_map:
+            term_map[term] += 1
+        else:
+            term_map[term] = 1
+        
+    
+    new_document = {
+        "_id" : docId,
+        docTitle : {
+            "text" : docText,
+            "num_chars" : num_chars,
+            "date" : docDate,
+            "category" : docCat,
+            "terms" : term_map
+        }
+    }
+    
+    cur.insert_one(new_document)
 
-    # 3 Update the potential new terms.
-    # 3.1 Find all terms that belong to the document. Use space " " as the delimiter character for terms and Remember to lowercase terms and remove punctuation marks.
-    # 3.2 For each term identified, check if the term already exists in the database
-    # 3.3 In case the term does not exist, insert it into the database
-    # --> add your Python code here
-
-    # 4 Update the index
-    # 4.1 Find all terms that belong to the document
-    # 4.2 Create a data structure the stores how many times (count) each term appears in the document
-    # 4.3 Insert the term and its corresponding count into the database
-    # --> add your Python code here
 
 def deleteDocument(cur, docId):
 
-    # 1 Query the index based on the document to identify terms
-    # 1.1 For each term identified, delete its occurrences in the index for that document
-    # 1.2 Check if there are no more occurrences of the term in another document. If this happens, delete the term from the database.
-    # --> add your Python code here
-
-    # 2 Delete the document from the database
-    # --> add your Python code here
+    
+    # delete document
+    delete_query = { "_id" : docId}
+    cur.delete_one(delete_query)
+    
 
 def updateDocument(cur, docId, docText, docTitle, docDate, docCat):
 
     # 1 Delete the document
     # --> add your Python code here
+    deleteDocument(cur, docId)
 
     # 2 Create the document with the same id
     # --> add your Python code here
+    createDocument(cur, docId, docText, docTitle, docDate, docCat)
+    
 
-def getIndex(cur):
+'''
+def getIndex():
 
     # Query the database to return the documents where each term occurs with their corresponding count. Output example:
     # {'baseball':'Exercise:1','summer':'Exercise:1,California:1,Arizona:1','months':'Exercise:1,Discovery:3'}
     # ...
     # --> add your Python code here
+    # 
+    # 
+'''
--- mongodb/index_mongo.py
+++ mongodb/index_mongo.py
@@ -1,14 +1,86 @@
-from pymongo import MongoClient  # import mongo client to connect
+from pymongo import MongoClient  # import mongo client to clientect
 import pprint
 import datetime
+
+from db_connection_mongo import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # Creating an instance of mongoclient and informing the connection string
-    client = MongoClient(host=['localhost:27017'])
-    # Creating database
-    db = client.library
+    # Creating an instance of mongoclient and informing the clientection string
+    cur = connectDataBase()
+    
+    
+    #print a menu
+    print("")
+    print("######### Menu ##############")
+    #print("#a - Create a category.")
+    print("#b - Create a document")
+    print("#c - Update a document")
+    print("#d - Delete a document.")
+    print("#e - Output the inverted index.")
+    print("#q - Quit")
+
+    option = ""
+    while option != "q":
+
+        print("")
+        option = input("Enter a menu choice: ")
+
+        '''
+        if (option == "a"):
+
+            catId = input("Enter the ID of the category: ")
+            catName = input("Enter the name of the category: ")
+
+            createCategory(catId, catName)
+            client.commit()
+        '''
+        
+        if (option == "b"):
+
+            docId = input("Enter the ID of the document: ")
+            docText = input("Enter the text of the document: ")
+            docTitle = input("Enter the title of the document: ")
+            docDate = input("Enter the date of the document: ")
+            docCat = input("Enter the category of the document: ")
+
+            createDocument(cur, docId, docText, docTitle, docDate, docCat)
+
+        elif (option == "c"):
+
+            docId = input("Enter the ID of the document: ")
+            docText = input("Enter the text of the document: ")
+            docTitle = input("Enter the title of the document: ")
+            docDate = input("Enter the date of the document: ")
+            docCat = input("Enter the category of the document: ")
+
+            updateDocument(cur, docId, docText, docTitle, docDate, docCat)
+
+
+        elif (option == "d"):
+
+            docId = input("Enter the document id to be deleted: ")
+
+            deleteDocument(cur, docId)
+
+
+        elif (option == "e"):
+
+            index = getIndex()
+            print(index)
+
+        elif (option == "q"):
+
+            print("Leaving the application ... ")
+
+        else:
+
+            print("Invalid Choice.")
+
+'''
+
+    
 
     #Creating a dictionary and include our data
     document = {"_id": 1,
@@ -32,3 +104,6 @@ if __name__ == '__main__':
     documents.delete_one({"_id": 1})
     # Fetching data
     pprint.pprint(documents.find_one())
+    
+    
+'''
